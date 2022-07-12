87f7d2ba4d3cbe5e0b28bae31b88fef56bdaffb9
aksdmi <aksdmi@gmail.com>
2022-07-12 09:52:23 +0300

    Make some cleaning

--- docmng/producer.py
+++ docmng/producer.py
@@ -1,11 +1,17 @@
 import pika, json
 
-params = pika.URLParameters('amqps://nafhfccg:kzeU7RMKLIuDasrfRh1_HlvSEdJelYKo@cow.rmq2.cloudamqp.com/nafhfccg')
+creds_broker = pika.PlainCredentials("queue_user", "Q123456q")
+conn_params = pika.ConnectionParameters(host="127.0.0.1", port=5672,
+                                        virtual_host = "/",
+                                        credentials = creds_broker)
 
-connection = pika.BlockingConnection(params)
+connection = pika.BlockingConnection(conn_params)
 
 channel = connection.channel()
 
 def publish(method, body):
     properties = pika.BasicProperties(method)
-    channel.basic_publish(exchange='', routing_key='admin', body=json.dumps(body), properties=properties)
+    channel.basic_publish(exchange='docaccmng_main',
+                          routing_key='accmng',
+                          body=json.dumps(body),
+                          properties=properties)
